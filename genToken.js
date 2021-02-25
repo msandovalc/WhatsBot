34c5ea192beb4de3d1fe1488e850ea586e201240
Manuel Sandoval Curiel <msandovalgm@live.com.mx>
2021-02-24 23:07:20 -0600

    fix get QR promise

--- genToken.js
+++ genToken.js
@@ -5,12 +5,12 @@ const fs = require('fs');
 const client = new Client({ puppeteer: { headless: true, args: ['--no-sandbox'] } });
 client.initialize();
 
-client.on('qr', (qr) => {
+client.on('qr', async(qr) => {
     console.log(`Scan this QR Code and copy the JSON\n`)
     qrcode.generate(qr, { small: true });
 });
 
-client.on('authenticated', (session) => {
+client.on('authenticated', async(session) => {
     console.log(JSON.stringify(session))
     fs.readFile(__dirname + '/session.json', { encoding: 'utf8' }, function(err, data) {
         if (err) {
